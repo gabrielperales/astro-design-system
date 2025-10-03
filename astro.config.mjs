97c70289074d888b0bbb678142cdb459862e9e54
Gabriel Perales <gabrielperales@gmail.com>
2025-10-03 13:18:28 +0200

    Update Astro configuration to include Preact and React integrations, set site URL, and upgrade dependencies. Change SCSS import to use syntax. Update package versions in package.json and yarn.lock for compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com',
   integrations: [
-    // Enable Preact to support Preact JSX components.
     vue(),
+    preact(),
+    react(),
     tailwind(),
+    mdx(),
   ],
 });
